e172d03acae3d40bd12425d52a82556e990cea46
Divyansh Kumar <divyanshkumar@Divyanshs-MacBook-Air.local>
2024-04-13 13:23:30 +0530

    first commit

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -33,6 +33,11 @@ const navConfig = [
     icon: icon('ic_lock'),
   },
   {
+    title: 'projects',
+    path: '/projects',
+    icon: icon('ic_lock'),
+  },
+  {
     title: 'Not found',
     path: '/404',
     icon: icon('ic_disabled'),
