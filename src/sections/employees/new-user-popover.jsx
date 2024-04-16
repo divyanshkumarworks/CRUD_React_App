d20da945281d15d8c334914fec048a9e1386a068
Divyansh Kumar <divyanshkumar@Divyanshs-MacBook-Air.local>
2024-04-17 03:48:24 +0530

    created employees page

---
+++ src/sections/employees/new-user-popover.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react'
+
+import TextField from '@mui/material/TextField';
+
+import Popover from '@mui/material/Popover';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
+import Autocomplete from '@mui/material/Autocomplete';
+import supabase from 'src/config/supabaseClient';
+
+
+export default function NewUserPopover ({
+    fetchEmployees, 
+    handleClose, 
+    open
+}) {
+    const [formData, setFormData] = useState({
+        name:'',
+        title:''    
+    });
+    const [fetchError, setFetchError] = useState(null);
+    const [projects, setProjects] = useState(null);
+
+    const [selectedProject, setSelectedProject] = useState({id:'', title:''})
+    
+    const handleSelectedProject = (e, selectedValue) => {
+        setSelectedProject(selectedValue)
+    } 
+
+    const handleChange = (e) => {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value
+        });
+    };
+
+    useEffect(() => {
+        fetchProjects()
+      }, [projects])
+    
+      const fetchProjects = async () => {
+        const { data, error } = await supabase
+        .from('Projects')
+        .select()
+    
+        if (error) {
+            setFetchError('could not fetch the projects')
+            setProjects(null)
+            console.log(error)
+        }
+    
+        if (data) {
+            setProjects(data)
+            setFetchError(null)
+        }
+      } 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const {data, error} = await supabase
+        .from('Employees')
+        .insert({name: formData.name, project_id: (selectedProject)? selectedProject.id: null})
+        .select()
+    
+        if(error) {
+        alert(error)
+        }
+        if(data) {
+          console.log("asdasd", data)
+          setSelectedProject(null)  
+          setFormData(null)
+          handleClose();
+          fetchEmployees();
+        }
+    }
+
+  return (
+    <Popover
+        open={!!open}
+        anchorEl={open}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={{ top: 300, left: 700 }}
+        anchorOrigin={{ vertical: 'center', horizontal: 'center' }}
+        transformOrigin={{ vertical: 'center', horizontal: 'center' }}
+        PaperProps={{
+        sx: {
+            p: 2,
+            mt: 1,
+            ml: 0.75,
+            width: 500,
+        },
+        }}
+    >
+        <form onSubmit={handleSubmit}>
+        <Container fixed>
+            <Stack spacing={3} mb={2}>
+
+            <TextField name="name" label="Enter Employee Name" onChange={handleChange}/>
+            
+            <Autocomplete
+            options={(projects || []).map((value) => ({ id: value.id, title: value.title }))}
+            getOptionLabel={(option)=>option.title}
+            isOptionEqualToValue={(option, value)=> option.value === value.value}
+            value={selectedProject}
+            onChange={handleSelectedProject}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="Select Project from Employee" />}
+            />
+
+            </Stack>
+            <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            >
+            create project
+            </LoadingButton>
+        </Container>
+        </form>
+        
+    </Popover>
+  )
+}
---
+++ src/sections/employees/view/employee-view.jsx
@@ -0,0 +1,231 @@
+import { useState, useEffect } from 'react';
+
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import Table from '@mui/material/Table';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import TableBody from '@mui/material/TableBody';
+import Typography from '@mui/material/Typography';
+import TableContainer from '@mui/material/TableContainer';
+import TablePagination from '@mui/material/TablePagination';
+
+import supabase from 'src/config/supabaseClient';
+
+import Iconify from 'src/components/iconify';
+import Scrollbar from 'src/components/scrollbar';
+import label from 'src/components/label';
+
+import NewUserPopover from '../new-user-popover';
+
+import TableNoData from '../table-no-data';
+import UserTableRow from '../user-table-row';
+import UserTableHead from '../user-table-head';
+import TableEmptyRows from '../table-empty-rows';
+import UserTableToolbar from '../user-table-toolbar';
+import { emptyRows, applyFilter, getComparator } from '../utils';
+
+// ----------------------------------------------------------------------
+
+export default function EmployeePage() {
+  const [page, setPage] = useState(0);
+
+  const [order, setOrder] = useState('asc');
+
+  const [selected, setSelected] = useState([]);
+
+  const [orderBy, setOrderBy] = useState('name');
+
+  const [filterName, setFilterName] = useState('');
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const [fetchError, setFetchError] = useState(null);
+  const [employees, setEmployees] = useState(null);
+
+  const [open, setOpen] = useState(null);
+
+  useEffect(() => {
+
+    fetchEmployees()
+  }, [employees])
+
+  const fetchEmployees = async () => {
+    const { data, error } = await supabase
+    .from('Employees')
+    .select(`
+      id,
+      name,
+      Projects (
+        title
+      )
+    `)
+
+    if (error) {
+        setFetchError('could not fetch the projects')
+        setEmployees(null)
+    }
+
+    if (data) {
+        setEmployees(data)
+        setFetchError(null)
+    }
+  }
+
+  const handleSort = (event, id) => {
+    const isAsc = orderBy === id && order === 'asc';
+    if (id !== '') {
+      setOrder(isAsc ? 'desc' : 'asc');
+      setOrderBy(id);
+    }
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked && employees) {
+      const newSelecteds = employees.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleDelete = async (e) => {
+    const {data, error} = await supabase
+        .from('Employees')
+        .delete()
+        .eq('name', selected)
+  
+      if (error) {
+        alert(error)
+      }
+  
+      if (data) {
+        console.log(data)
+        fetchEmployees();
+      }
+  }
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPage(0);
+    setRowsPerPage(parseInt(event.target.value, 10));
+  };
+
+  const handleFilterByName = (event) => {
+    setPage(0);
+    setFilterName(event.target.value);
+  };
+
+  const dataFiltered = applyFilter({
+    inputData: (employees !== null) ? employees : [],
+    comparator: getComparator(order, orderBy),
+    filterName,
+  });
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+
+  const handleOpen = (event) => {
+    setOpen(event.currentTarget);
+  };
+
+  const notFound = !dataFiltered.length && !!filterName;
+  return (
+    <Container>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Typography variant="h4">Employees</Typography>
+
+        <Button variant="contained" color="inherit" onClick={handleOpen} startIcon={<Iconify icon="eva:plus-fill" />}>
+          New User
+        </Button>
+      </Stack>
+
+      <NewUserPopover 
+        fetchEmployees={fetchEmployees}
+        handleClose={handleClose}
+        open={open}
+      />
+
+      <Card>
+        <UserTableToolbar
+          numSelected={selected.length}
+          filterName={filterName}
+          onFilterName={handleFilterByName}
+        />
+
+        <Scrollbar>
+          <TableContainer sx={{ overflow: 'unset' }}>
+            <Table sx={{ minWidth: 800 }}>
+              <UserTableHead
+                order={order}
+                orderBy={orderBy}
+                rowCount={employees !== null ? employees.length: null}
+                numSelected={selected.length}
+                onRequestSort={handleSort}
+                onSelectAllClick={handleSelectAllClick}
+                headLabel={[
+                  { id: 'name', label: 'Name' },
+                  { id: 'project', label: 'Project' },
+                  { id: '', label: ''},
+                ]}
+              />
+              <TableBody>
+                {dataFiltered
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => (
+                    <UserTableRow
+                      key={row.id}
+                      name={row.name}
+                      project_name={(row.Projects !== null) ? row.Projects.title: "" }
+                      project_id={row.id}
+                      selected={selected.indexOf(row.name) !== -1}
+                      handleClick={(event) => handleClick(event, row.name)}
+                    />
+                  ))}
+
+                <TableEmptyRows
+                  height={77}
+                  emptyRows={emptyRows(page, rowsPerPage, employees !== null ? employees.length : null)}
+                />
+
+                {notFound && <TableNoData query={filterName} />}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Scrollbar>
+
+        <TablePagination
+          page={page}
+          component="div"
+          count={employees !== null ? employees.length: 0}
+          rowsPerPage={rowsPerPage}
+          onPageChange={handleChangePage}
+          rowsPerPageOptions={[5, 10, 25]}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Card>
+    </Container>
+  );
+}
